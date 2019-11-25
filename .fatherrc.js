7e83ead5c90333521e903d0e3a146ec7f5c5f019
jiangkun13 <jiangkun13@163.com>
2019-11-25 16:29:44 +0800

    0.0.3

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,4 +1,14 @@
 export default {
   cjs: 'babel',
   esm: 'babel',
+  "plugins": [
+    [
+      "import",
+      {
+        "libraryName": "antd",
+        "libraryDirectory": "es",
+        "style": "css"
+      }
+    ]
+  ]
 }
